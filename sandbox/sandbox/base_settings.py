40561e4fd963f16a3dee6fcd63af0a99d9a923e6
Vladislav Stashevskiy <fnsdevelopment@gmail.com>
2019-06-14 13:07:41 +0300

    Settings were split

---
+++ sandbox/sandbox/base_settings.py
@@ -0,0 +1,109 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Application definition
+
+INSTALLED_APPS = [
+    'core',
+    'user',
+    'github_integration',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crispy_forms',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'sandbox.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'sandbox.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# User model
+
+AUTH_USER_MODEL = 'user.User'
+
+# Backends
+
+AUTHENTICATION_BACKENDS = [
+    'user.backends.LoginByEmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+# Login URL
+
+LOGIN_URL = '/user/login'
+
+# Github integration
+
+GITHUB_SCOPE = 'repo user'
+GITHUB_LOGIN_URL = 'https://github.com/login/oauth/authorize'
+GITHUB_GET_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+GITHUB_APPLICATIONS_URL = 'https://api.github.com/applications'
+GITHUB_REPOS_URL = 'https://api.github.com/repos'
+GITHUB_USERS_URL = 'https://api.github.com/users'
+GITHUB_USER_URL = 'https://api.github.com/user'
